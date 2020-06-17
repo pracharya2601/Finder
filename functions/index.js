a7cbb8d713eabcc2ff6a6ed6892bc44266e67b1e
Prakash Acharya <pracharya2601@gmail.com>
2020-06-17 15:55:42 -0700

    notification trigger added

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,23 @@
 const functions = require('firebase-functions');
 
+const { db } = require('./util/admin');
+
 //import routes
-const  { 
-    getAllPlaces, 
-    postOnePlace,
-    getPlace,
-    commentOnPlace,
-    likePlace,
-    unlikePlace,
-    deletePlace
+const {
+  getAllPlaces,
+  postOnePlace,
+  getPlace,
+  commentOnPlace,
+  likePlace,
+  unlikePlace,
+  deletePlace,
 } = require('./handlers/places');
-const { 
-    signup, 
-    login, 
-    uploadImage, 
-    addUserDetails, 
-    getAuthenticatedUser 
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
 } = require('./handlers/users');
 
 //import firebae auth
@@ -25,7 +27,6 @@ const FBAuth = require('./util/fbAuth');
 const express = require('express');
 const app = express();
 
-
 // places routes
 app.get('/places', getAllPlaces);
 app.post('/place', FBAuth, postOnePlace);
@@ -34,19 +35,82 @@ app.get('/place/:placeId', getPlace);
 app.delete('/place/:placeId', FBAuth, deletePlace);
 app.get('/place/:placeId/like', FBAuth, likePlace);
 app.get('/place/:placeId/unlike', FBAuth, unlikePlace);
-app.post('/place/:placeId/comment', FBAuth, commentOnPlace)
-
-
+app.post('/place/:placeId/comment', FBAuth, commentOnPlace);
 
 //user routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth,  uploadImage);
+app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser)
-
-
-
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 //https://baseurl.com/api
 exports.api = functions.https.onRequest(app);
+
+//like notification
+
+exports.createNotificationOnLike = functions.firestore
+  .document('likes/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/places/${snapshot.data().placeId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            placeId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions.firestore
+  .document('likes/{id}')
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document('comments/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/places/${snapshot.data().placeId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            placeId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
